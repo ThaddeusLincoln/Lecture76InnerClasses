36a6f05202cad863d18809e579bc16562f1707ed
ThaddeusLincoln <beny1419@yahoo.es>
2016-08-15 13:58:52 +0200

    Making an InnerClass private is a good programming practice, because no one else needs to know about an InnerClass besides its outer class.

--- Lect76InnerClasses/src/com/turing/jdev/innerclasses/Main.java
+++ Lect76InnerClasses/src/com/turing/jdev/innerclasses/Main.java
@@ -4,10 +4,21 @@ public class Main {
 
 	public static void main(String[] args){
 		
-		// 1. Sample of using a non-static nested class, or Inner class
+		// 1. Sample of using a non-static nested class, or Inner class which is private now
 		Gearbox mcLaren = new Gearbox(6);
-		Gearbox.Gear first = mcLaren.new Gear(1, 12.3);
-		System.out.println(first.driverSpeed(100));
+		
+		/*mcLaren.addGear(1, 5.3);
+		mcLaren.addGear(2, 10.6);
+		mcLaren.addGear(3, 15.9);*/
+		
+		
+		mcLaren.operateClutch(true);
+		mcLaren.changeGear(1);
+		mcLaren.operateClutch(false);
+		System.out.println(mcLaren.wheelSpeed(1000));
+		mcLaren.changeGear(2);
+		System.out.println(mcLaren.wheelSpeed(3000));
+		
 
 	}
 }
